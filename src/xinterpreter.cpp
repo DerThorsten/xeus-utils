929926321e01e7f1e49c59f26ceae8c17b657894
DerThorsten <derthorstenbeier@gmail.com>
2022-02-07 14:55:35 +0100

    updated install targets

--- src/xinterpreter.cpp
+++ src/xinterpreter.cpp
@@ -39,7 +39,7 @@ namespace xeus_utils
 
         if (code.compare("hello, world") == 0)
         {
-            publish_stream("stdout", code);
+            publish_stream("stdout", code+ std::string("bazinga!!"));
         }
 
         if (code.compare("error") == 0)
